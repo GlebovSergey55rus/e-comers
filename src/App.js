21e7ea22e84d563cbb9cf8826d23de867e1b8c62
GlebovSergey55rus <124247632+GlebovSergey55rus@users.noreply.github.com>
2023-02-06 12:00:58 +0300

    Загрузил файлы

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+<body>
+<header>
+  <div>
+      <div class="header">
+        <div class="left">
+          <a href="#">City</a>
+        </div>
+        <div class="right">
+          <a href='#'>Document</a>
+          <a href="#">Contact</a>
+          <a href="#">Download</a>
+        </div>
+      </div> 
+  </div>   
+  <div>
+    <div class="main">
+          <div class="pipl column">
+            <a href="#">Женщинам</a>
+            <a href="#">Мужчинам</a>
+            <a href="#">Детям</a>
+          </div>
+          <div class="logo column">
+             <a href="#">E-comers</a>
+          </div>
+          <div class="entrance column">
+             <a href='#'>Войти</a>
+             <a href="#">Корзина</a>
+          </div>
+    </div>
+  </div>
+  <div>
+    <div class="collum">
+      <div class="sale">
+        <a href="#">Идеи</a>
+        <a href="#">Новинки</a>
+        <a href="#">Одежда</a>
+        <a href="#">Обувь</a>
+        <a href="#">Аксесуары</a>
+        <a href="#">Бренды</a>
+        <a href="#">Premium</a>
+        <a href="#">Спорт</a>
+        <a href="#">Красота</a>
+        <a href="#">Дом</a>
+        <a class="redy" href="#">Sale%</a>
+      </div>
+      <div class="poisk">
+        <input type="search" name='poisk' placeholder='поиск'/>
+        <input type="submit" value="искать"/>
+      </div>
+    </div>
+  </div>    
+</header>       
+<main> 
+  <div>
+    <div class="row">
+        <h1 class="chapter">Выберите интересующий вас раздел</h1>
+    </div>
+  </div>
+  <div> 
+    <div class="images">
+      <div class="img1">
+      <a href="#"><img src='https://i.pinimg.com/originals/fe/04/e6/fe04e638c3e51a43709501b144cbbc3f.jpg' alt=''/>
+      </a>
+      <div>Мужчинам</div>
+      </div> 
+      <div class="img2">
+      <a href="#"><img src='https://cameralabs.org/media/camera/molotov/23-08/supprealisticheskoe-foto-detey_12.jpg' alt=''/></a>
+      </div> 
+      <div class="img3">
+      <a href="#"><img src='https://99px.ru/sstorage/56/2015/02/image_5613021523532446020.jpg' alt=''/></a>
+      </div> 
+    </div>   
+  </div>   
+  <div> 
+    <div class="infa">
+      <div class="block1">
+      <h2>Доставка на следующий день.</h2>
+      <p>В Москве и 60 других крупных городах России вы получите свой заказ уже на следующий день! Более подробную информацию об условиях доставки в ваш город можно найти здесь.</p>
+      </div> 
+      <div class="block2">
+      <h2>Примерка перед покупкой.</h2>
+      <p>Интернет-магазин Lamoda даёт возможность примерить одежду, обувь и другие товары перед оплатой заказа курьеру. Оплачивайте только то, что вам подошло и понравилось!</p>
+      </div> 
+      <div class="block3">
+      <h2>Удобные способы оплаты.</h2>
+      <p>Вы можете оплатить покупки не только наличными, но и банковской картой. У всех курьеров Lamoda Express при себе есть терминал для оплаты картами.</p>
+      </div> 
+    </div>   
+  </div> 
+</main>
+
+  <div class="nara">
+  <p>hkjghgffgjgkughjhj</p>
+  </div>
+
+<footer>
+
+</footer>
+</body>
+</>
+  );
+}
+
+export default App;
